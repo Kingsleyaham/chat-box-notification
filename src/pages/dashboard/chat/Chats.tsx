f4b25bdc4a3cb83a6dd753599670279186c5318a
Aham Kingsley <kingsleyaham6@gmail.com>
2023-11-17 06:31:56 +0100

    fix: resize chat area width to full

--- src/pages/dashboard/chat/Chats.tsx
+++ src/pages/dashboard/chat/Chats.tsx
@@ -37,7 +37,7 @@ const Chats = () => {
           )}
         </div>
       </div>
-      <div className="scrollable-content h-[60vh] xl:h-[65vh] 2xl:h-[70vh] w-11/12 lg:w-10/12 mx-auto">
+      <div className="scrollable-content h-[60vh] xl:h-[65vh] 2xl:h-[70vh] w-full mx-auto">
         <div className="flex items-center gap-4 bg-[#fafafa] shadow-lg  mt-1 px-6 py-4 w-[97%]">
           <div>
             <img src={Apartment} alt="" width="110px" height="80px" />
