1c0f1f9aad6d9b28b8e662bc9295d1967d9f22e9
Aham Kingsley <kingsleyaham6@gmail.com>
2023-11-19 13:16:26 +0100

    .

--- src/pages/dashboard/profile/ProfileMobileNav.tsx
+++ src/pages/dashboard/profile/ProfileMobileNav.tsx
@@ -9,7 +9,7 @@ type PropTypes = {
 
 const ProfileMobileNav = ({ setMenuIsOpen, menuIsOpen }: PropTypes) => {
   return (
-    <div className=" px-8 md:px-12 py-1 z-50 bg-white fixed w-full">
+    <div className=" px-8 md:px-12 py-1 z-50 bg-white top-0 fixed w-full">
       <div className="flex justify-between items-center py-3">
         <button onClick={() => setMenuIsOpen(!menuIsOpen)} className="cursor-pointer">
           {menuIsOpen ? <IoClose size="2rem" /> : <HamburgerIcon />}
